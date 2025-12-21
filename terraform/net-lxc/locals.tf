d5c1e9fdeae2ea91c502e3fcc4afd9560d1119c3
Israel Farfan <israel.farfan@gmx.com>
2025-12-21 11:39:37 -0500

    tailscale update

---
+++ terraform/net-lxc/locals.tf
@@ -0,0 +1,32 @@
+locals {
+  hostname     = "net-lxc"
+  description  = "net LXC"
+  cores        = 4
+  memory       = 8192
+  onboot       = true
+  unprivileged = false # privileged = true
+  tags         = "net"
+  ip           = "192.168.136.23"
+
+  # Disks
+  rootfs_size = "30G"
+  mountpoint = [
+    {
+      mp   = "/var/lib/docker"
+      size = "8G"
+    },
+    {
+      mp   = "/opt"
+      size = "50G"
+    }
+  ]
+
+  # Launch in this Proxmox node
+  target_node = "macmini1"
+
+  # Additional DNS aliases/CNAMEs
+  dns_aliases = [
+    "traefik-net-lxc",
+    "portainer-net-lxc",
+  ]
+}
