58ec626784c2914ff4312b55b54054e78298fd58
Israel Farfan <israel.farfan@gmx.com>
2026-01-05 13:21:13 -0500

    add cnames

--- terraform/rpi/vars.tf
+++ terraform/rpi/vars.tf
@@ -12,17 +12,34 @@ locals {
       hostname    = "rpi0",
       description = "RaspberryPi DNS 0",
       ip          = "192.168.136.2"
+      aliases = [
+        "traefik-rpi0",
+      ]
     },
     {
       hostname    = "rpi1",
       description = "RaspberryPi DNS 1",
       ip          = "192.168.136.3"
+      aliases = [
+        "traefik-rpi1",
+        "pulse"
+      ]
     }
   ]
 
-  # ? DNS record defaults
-  dns_record = {
-    ttl  = 3600,
-    type = "A"
+  # Flattened list of CNAME records so we can create them in a single resource
+  cname_records_list = flatten([
+    for rpi in local.rpis : [
+      for alias in rpi.aliases : {
+        key          = "${rpi.hostname}-${alias}"
+        cname_record = alias
+        cname_value  = rpi.hostname
+      }
+    ]
+  ])
+
+  cname_records_map = {
+    for instance in local.cname_records_list :
+    instance.key => instance
   }
 }
